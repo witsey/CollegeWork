4f79469034f892d7b1c9d4658c891f7627609c83
Akram <tubeakram7@gmail.com>
2023-10-28 22:40:00 +0200

    Fixed VScode and CodeBlocks
    Deleted some files in CodeBlocks Projects + Finally fixed VScode

--- Vscode/WorkJHoursCalc.cpp
+++ Vscode/WorkJHoursCalc.cpp
@@ -5,26 +5,19 @@
 using namespace std;
 
 int main() {
+
+    bool red{false};
+    bool green{false};
     
-    int work_hours;
-    int regular_earning;
-    int overtime_hours;
-    int total_earning;
-    int overtime_earning;
+    if (red){
 
-    overtime_earning = 050;
+        cout << "Hello";
+    }else{
+
+        cout << "No Hello";
+    };
     
-    cout << "Enter work hours: ";
-    cin  >> work_hours;
-    regular_earning = work_hours * 18.25;
 
-    if ( work_hours > 40 ) {
-        overtime_hours = work_hours - 40;
-        overtime_earning = overtime_hours * 27.78;
 
-    }
-
-    total_earning = overtime_earning + regular_earning;
-    cout << "The Following Employee Earning is: " << total_earning << "$";
-
+    return 0;
 }
