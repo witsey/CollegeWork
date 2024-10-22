cabdcf452287fd6f3472e7b080391a49c01bef10
Akram <121326414+witsey@users.noreply.github.com>
2024-10-23 00:07:18 +0300

    Implemented LinkedList Class
    - Added a LinkedList class template to
      manage a list of Node objects.
    
    - Implemented the following methods
    in LinkedList:
    
      - add_to_head(type val): Adds a new
        node with the given value to the
        head of the list.
    
      - add_to_tail(type val): Adds a new
        node with the given value to the tail
        of the list.
    
      - add_to_middle(type val, int index):
        Adds a new node with the given
        value at the specified index.
    
    - Demonstrated the usage of LinkedList in the main function:
    
      - Created a LinkedList of integers.
      - Added nodes to the head and tail.
      - Printed all node values to verify the
         list's contents.
    
    - Updated the Node class to include a constructor for initializing the node's value and next pointer.
    
    - Added a destructor to the LinkedList class for cleanup.

--- Year3/DataStructs/lec8/SLL.cpp
+++ Year3/DataStructs/lec8/SLL.cpp
@@ -16,13 +16,65 @@ class Node
         }
 };
 
+template <typename type>
+class LinkedList 
+{
+    private:
+
+    public:
+        Node<type> *head, *tail;
+        LinkedList() { this->head = this->tail = nullptr; }
+        
+        void add_to_head(type val) 
+        {
+            if (head == nullptr) { this->head = this->tail = new Node<type>(val); }
+
+            else 
+            {
+                Node<type>* newNode = new Node<type>(val, head);
+                this->head = newNode;
+            }
+        }
+
+        void add_to_tail(type val) 
+        {
+            if (tail == nullptr) { this->head = this->tail = new Node<type>(val); }
+
+            else
+            {
+                this->tail->next = new Node<type>(val);
+                this->tail = this->tail->next;
+            }
+        }
+
+        void add_to_middle(type val, int index) 
+        {
+            int ctr = -1;
+            Node<type>* current = this->head;
+
+            while (current && ++ctr < index) {current = current->next;}
+
+            if (current == this->head) { add_to_head(val); return; }
+
+            Node<type>* newNode = new Node<type>(val, current->next);
+            current->next = newNode;
+        }
+
+        ~LinkedList() {delete this->head; delete this->tail;}
+};
+
 int main() 
 {
-    Node<int>* head = new Node<int>(3);
-    head->next = new Node<int>(4);
-    head->next->next = new Node<int>(5);
+    // Node<int>* head = new Node<int>(3);
+    // head->next = new Node<int>(4);
+    // head->next->next = new Node<int>(5);
 
-    Node<int>* cur = head;
+    LinkedList<int> l1;
+    l1.add_to_head(1);
+    l1.add_to_tail(2);
+    l1.add_to_tail(3);
+
+    Node<int>* cur = l1.head;
 
     while (cur != nullptr) 
     {
